7d3d84fba0ac2fb078179757e9b752ef0ce9fe12
jwest650 <jaywest650@gmail.com>
2023-05-31 15:25:20 +0000

    instance

--- components/utils/axiosInstance.tsx
+++ components/utils/axiosInstance.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+let url =
+     process.env.NODE_ENV === "production"
+          ? `${process.env.HOST}`
+          : "http://localhost:5000";
 export const instance = axios.create({
-     baseURL:
-          process.env.NODE_ENV === "production"
-               ? `${process.env.HOST}`
-               : "http://localhost:5000",
+     baseURL: "http://localhost:5000",
 });
